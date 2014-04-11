563698fdf5b561eaa12f08f952662ceaeb2ca24e
Rik Goldman <rikgoldman@gmail.com>
2014-04-11 10:08:52 -0400

    main change is renaming index to main.php

--- main.php
+++ main.php
@@ -1,3 +1,8 @@
+<?php 
+
+require_once 'includes/navigation_functions.php';
+require_once 'includes/metadata_functions.php';
+?>
 <!DOCTYPE html>
 
 
@@ -6,7 +11,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     
     <title>Chelsea School &mdash; Chelsea School Mobile</title>
-    
+    <link rel="stylesheet" href="css/tabs.min.css" type="text/css" />
     <link rel="stylesheet" href="_static/basic.css" type="text/css" />
     <link rel="stylesheet" href="_static/pygments.css" type="text/css" />
     <link rel="stylesheet" href="_static/bootstrap-3.0.0/css/bootstrap.min.css" type="text/css" />
@@ -22,6 +27,7 @@
         HAS_SOURCE:  true
       };
     </script>
+    <script type="text/javascript" src="js/tabs.min.js"></script>
     <script type="text/javascript" src="_static/jquery.js"></script>
     <script type="text/javascript" src="_static/underscore.js"></script>
     <script type="text/javascript" src="_static/doctools.js"></script>
@@ -30,12 +36,14 @@
     <script type="text/javascript" src="_static/bootstrap-3.0.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="_static/bootstrap-sphinx.js"></script>
     <link rel="author" title="About these documents" href="about.php" />
-    <link rel="top" title="Chelsea School Mobile" href="index.php" />
+    <link rel="top" title="Chelsea School Mobile" href="#" />
+    <link rel="next" title="About Chelsea School" href="about.php" />
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1'>
 <meta name="apple-mobile-web-app-capable" content="yes">
-
+<?php write_bottom_padding(); ?>
+<?php write_top_padding(); ?>
   </head>
   <body>
 
@@ -48,15 +56,24 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.php">Chelsea School</a>
+        <a class="navbar-brand" href="#">Chelsea School</a>
         <span class="navbar-text navbar-version pull-left"><b></b></span>
       </div>
 
         <div class="collapse navbar-collapse nav-collapse">
           <ul class="nav navbar-nav">
+            <li class="divider-vertical"></li>
+            
+                <li><a href=".php"></a></li>
+                <li><a href="./"></a></li>
             
             
-       
+              <li class="dropdown globaltoc-container">
+  <a href="#"
+     class="dropdown-toggle"
+     data-toggle="dropdown">Site <b class="caret"></b></a>
+  <ul class="dropdown-menu globaltoc"
+    ><ul>
 <li class="toctree-l1"><a class="reference internal" href="about.php">About Chelsea School</a><ul>
 <li class="toctree-l2"><a class="reference internal" href="about.php#philosophy">Philosophy</a></li>
 <li class="toctree-l2"><a class="reference internal" href="about.php#history">History</a></li>
@@ -92,7 +109,13 @@
 </ul>
 </ul>
 </li>
-</ul>
+              
+            
+            
+            
+            
+            
+          </ul>
 
           
             
@@ -111,34 +134,77 @@
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-  <!-- Nav tabs -->
+    <!-- Nav tabs -->
 	<div class="nav">
 	<ul class="nav nav-tabs">
- 		 <li class="active"><a href="#buttons" data-toggle="tab">Buttons</a></li>
- 		 <li><a href="#contents" data-toggle="tab">Contents</a></li>
-		  <li><a href="#history" data-toggle="tab">History</a></li>
+ 		 <li class="active"><a href="#buttons" data-toggle="tab">Main Menu</a></li>
+ 		 <li><a href="#contents" data-toggle="tab">Detailed Contents</a></li>
+		  
 		  
 	</ul>
 	</div>
  <!-- end tabs -->
-      
-  <div class="section" id="chelsea-school">
-<h1>Chelsea School<a class="headerlink" href="#chelsea-school" title="Permalink to this headline">¶</a></h1>
-<div class="section" id="image-placeholder">
-<h2>Image Placeholder<a class="headerlink" href="#image-placeholder" title="Permalink to this headline">¶</a></h2>
+ <div class="tab-content">
+ <div class="tab-pane active" id="buttons">
+ 
+<p align=center><img src="http://placehold.it/350x150"></p>
+<button type="button" class="btn btn-primary btn-lg btn-block" name="buttons" type="button" href="about.php">About Chelsea School</a>
+<button type="button" class="btn btn-primary btn-lg btn-block" type="button" href="admissions.php">Admissions</a>
+<button type="button" class="btn btn-primary btn-lg btn-block" type="button" href="families.php">Families</a>
+<button type="button" class="btn btn-primary btn-lg btn-block" type="button" href="calendar.php">Calendars</a> 
 </div>
-<div class="section" id="main-buttons">
-<h2>Main Buttons<a class="headerlink" href="#main-buttons" title="Permalink to this headline">¶</a></h2>
-</div>
-</div>
+ 
+<div class="tab-pane" id="contents">     
+ <h1>Chelsea School<a name="contents" class="headerlink" href="#chelsea-school" title="Permalink to this headline">¶</a></h1>
+<!-- <div class="toctree-wrapper compound">-->
+<ul>
+<li class="toctree-l1"><a class="reference internal" href="about.php">About Chelsea School</a><ul>
+<li class="toctree-l2"><a class="reference internal" href="about.php#philosophy">Philosophy</a></li>
+<li class="toctree-l2"><a class="reference internal" href="about.php#history">History</a></li>
+<li class="toctree-l2"><a class="reference internal" href="about.php#news">News</a></li>
+</ul>
+</li>
+<li class="toctree-l1"><a class="reference internal" href="meet.php">Meet Chelsea School</a><ul>
+<li class="toctree-l2"><a class="reference internal" href="meet.php#students">Students</a></li>
+<li class="toctree-l2"><a class="reference internal" href="meet.php#board">Board</a></li>
+<li class="toctree-l2"><a class="reference internal" href="meet.php#administration">Administration</a></li>
+<li class="toctree-l2"><a class="reference internal" href="meet.php#faculty">Faculty</a></li>
+</ul>
+</li>
+<li class="toctree-l1"><a class="reference internal" href="admissions.php">Admissions</a><ul>
+<li class="toctree-l2"><a class="reference internal" href="admissions.php#tours">Tours</a></li>
+<li class="toctree-l2"><a class="reference internal" href="admissions.php#visits">Visits</a></li>
+<li class="toctree-l2"><a class="reference internal" href="admissions.php#open-house-events">Open House Events</a></li>
+<li class="toctree-l2"><a class="reference internal" href="admissions.php#admissions-process">Admissions Process</a></li>
+</ul>
+</li>
+<li class="toctree-l1"><a class="reference internal" href="calendar.php">Calendars</a><ul>
+<li class="toctree-l2"><a class="reference internal" href="calendar.php#id1">Academic Calendar</a></li>
+<li class="toctree-l2"><a class="reference internal" href="calendar.php#id2">Athletic Calendar</a></li>
+<li class="toctree-l2"><a class="reference internal" href="calendar.php#id3">Events Calendar</a></li>
+</ul>
+</li>
+<li class="toctree-l1"><a class="reference internal" href="family.php">Family Resources</a><ul>
+<li class="toctree-l2"><a class="reference internal" href="family.php#id1">Moodle</a></li>
+<li class="toctree-l2"><a class="reference internal" href="family.php#id2">Purchase Lunch</a></li>
+<li class="toctree-l2"><a class="reference internal" href="family.php#id3">Chelsea School Technology Guide</a></li>
+</ul>
+</li>
+</ul>
 
-
+<!-- </div>-->
+<!--  <div class="section" id="indices-and-tables">-->
+<h1>Indices and tables<a class="headerlink" href="#indices-and-tables" title="Permalink to this headline">¶</a></h1>
+<ul class="simple">
+<li><a class="reference internal" href="genindex.php"><em>Index</em></a></li>
+<li><a class="reference internal" href="search.php"><em>Search Page</em></a></li>
+</ul>
+</div>
+</div>
+</div>
     </div>
       
   </div>
-</div>
-<div class="container">
-<a class="btn-large btn-block btn-primary" type="button" href="about.php">About Chelsea School</a>
 </div>
 <footer class="footer">
   <div class="container">
@@ -151,5 +217,14 @@
     </p>
   </div>
 </footer>
+<!-- Tabs -->
+    <script type="text/javascript">
+    $( document ).ready()
+	$('#buttons a[href="#buttons"]').tab('show') // Select tab by name
+    $('#contents a[href="#contents"]').tab('show') // Select tab by name
+    
+    
+</script>    
+    <!-- End Tabs -->
   </body>
 </html>

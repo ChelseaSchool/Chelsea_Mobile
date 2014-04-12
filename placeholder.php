0924376ab8bf44ad10c7cce6d74f72346d1d2c21
Rik Goldman <rikgoldman@gmail.com>
2014-04-12 07:16:04 -0400

    placeholder temporary and symbolic

---
+++ placeholder.php
@@ -0,0 +1,26 @@
+
+<?php
+
+require_once 'includes/navigation_functions.php';
+require_once 'includes/metadata_functions.php';
+?>
+
+<!DOCTYPE html>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <title>Placeholder</title>
+    
+   <?php write_depends(); ?>
+<?php write_bottom_padding(); ?>
+<?php write_top_padding(); ?>
+  </head>
+  <body>
+<?php write_top_navbar(); ?>    
+<p>Imagine marketing content here.</p>
+<?php write_footer(); ?>
+</body>
+</html>

3a401e1f67ad9646b9df57ba2fc099fc38691e8d
Rik Goldman <rikgoldman@gmail.com>
2014-07-17 11:05:47 -0400

    added social.php

---
+++ social.php
@@ -0,0 +1,75 @@
+<?php
+require_once 'includes/navigation_functions.php';
+require_once 'includes/metadata_functions.php';
+?>
+
+<!DOCTYPE html>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Chelsea School</title>
+
+
+<link rel="stylesheet" href="css/tabs.min.css" type="text/css" />
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+<!-- FontAwesome Resources -->
+<link
+    href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css"
+    rel="stylesheet">
+
+
+<!-- jQuery Resources -->
+<link rel="stylesheet"
+    href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="css/chelsea.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+<script type="text/javascript" src="js/tabs.min.js"></script>
+<script type="text/javascript" src="_static/jquery.js"></script>
+<script type="text/javascript" src="_static/underscore.js"></script>
+<script type="text/javascript" src="_static/doctools.js"></script>
+<script type="text/javascript" src="_static/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="_static/js/jquery-fix.js"></script>
+<script type="text/javascript"
+    src="_static/bootstrap-3.0.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="_static/bootstrap-sphinx.js"></script>
+
+<meta charset='utf-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+<meta name='viewport'
+    content='width=device-width, initial-scale=1.0, maximum-scale=1'>
+<meta name="apple-mobile-web-app-capable" content="yes">
+</head>
+<body>
+<?php write_top_navbar(); ?>
+<div class="container">
+<h1>Chelsea on the Internet</h1>
+<p title="placeholder">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<div class="row">
+
+<!-- column 1 -->
+<div class="col-xs-4 placeholder"><p><a type="button" class="btn btn-lg btn-default" href="http://facebook.com" title="Chelsea on Facebook" alt="Facebook"><i class="fa fa-facebook-square fa-5x"></i></p></a>
+
+</div>
+
+<!-- column 2 -->
+<div class="col-xs-4 placeholder"><p><a type="button" class="btn btn-lg btn-default" href="http://linkedin.com" title="Chelsea on LinkedIn" alt="LinkedIn"><i class="fa fa-linkedin-square fa-5x"></i></i></p></a>
+
+</div>
+
+<!-- column 3 -->
+<div class="col-xs-4 placeholder"><p><a type="button" class="btn btn-lg btn-default" href="http://facebook.com" title="Chelsea on Facebook" alt="Facebook"><i class="fa fa-facebook-square fa-5x"></i></a>
+
+</div>
+
+</div>
+</div>
+
+</body>
